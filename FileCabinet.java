c89ae0d4294d6ee879b915539f3064ab95524ef7
wkobierski <wiktor4.kobierski@gmail.com>
2024-08-30 16:51:22 +0200

    Add files via upload

--- FileCabinet.java
+++ FileCabinet.java
@@ -1,78 +1,101 @@
+package v4LepszyIterator;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
 interface Cabinet {
-    // zwraca dowolny element o podanej nazwie
     Optional<Folder> findFolderByName(String name);
-
-    // zwraca wszystkie foldery podanego rozmiaru SMALL/MEDIUM/LARGE
     List<Folder> findFoldersBySize(String size);
-
-    //zwraca liczbę wszystkich obiektów tworzących strukturę
     int count();
 }
 
-public class FileCabinet implements Cabinet {
+public class FileCabinet implements Cabinet, Iterable<Folder> {
     private List<Folder> folders;
 
-    private enum SearchType{
-        NAME, SIZE, COUNT
-    }
-
-    //zwraca dowolny folder o podanej nazwie lub Optional.empty jeżeli nie znaleziono żadnego folderu o podanej nazwie
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        List<Folder> resultList = exploreFolders(this.folders, SearchType.NAME, name, null);
-
-        return resultList.isEmpty() ? Optional.empty() : Optional.ofNullable(resultList.getFirst());
-    }
-
-    //zwraca wszystkie foldery o podanym rozmiarze
-    @Override
-    public List<Folder> findFoldersBySize(String size) {
-        return exploreFolders(this.folders, SearchType.SIZE, null, size);
-    }
-
-    //zwraca liczbę wszystkich obiektów tworzących strukturę
-    @Override
-    public int count() {
-        return exploreFolders(this.folders, SearchType.COUNT, null, null).size();
-    }
-
-
-    /*przeszukuje foldery w poszukiwaniu tych, które spełniają podany warunek
-    jeżeli napotkany folder jest Multifolderem, metoda rekurencyjnie przeszukuje Multifolder
-     */
-    private List<Folder> exploreFolders(List<Folder> folders, SearchType searchType, String name, String size){
-        List<Folder> resultList = new ArrayList<>();
-
-        if (folders == null)
-            return resultList;
-
-        for (Folder folder : folders){
-            if(folderMeetsCondition(folder, searchType, name, size)){
-                resultList.add(folder);
-            }
-
-            if(searchType == SearchType.NAME && !resultList.isEmpty())
-                return resultList;
-
-            if (folder instanceof MultiFolder){
-                resultList.addAll(exploreFolders(((MultiFolder) folder).getFolders(), searchType, name, size));
+        for (Folder folder : this) {
+            if (folder.getName().equals(name)) {
+                return Optional.of(folder);
             }
         }
+        return Optional.empty();
+    }
 
+    @Override
+    public List<Folder> findFoldersBySize(String size) {
+        List<Folder> resultList = new ArrayList<>();
+        for (Folder folder : this) {
+            if (folder.getSize().equals(size)) {
+                resultList.add(folder);
+            }
+        }
         return resultList;
     }
 
-    //sprawdzenie warunku dla folderu w metodzie exploreFolders, w zależności od rodzaju przeszukiwania
-    private boolean folderMeetsCondition(Folder folder, SearchType searchType, String name, String size) {
-        return switch (searchType) {
-            case NAME -> folder.getName().equals(name);
-            case SIZE -> folder.getSize().equals(size);
-            case COUNT -> true;
-        };
+    @Override
+    public int count() {
+        int count = 0;
+        for (Folder folder : this) {
+            count++;
+        }
+
+        return count;
+    }
+
+    @Override
+    public Iterator<Folder> iterator() {
+        return new FolderIterator(folders);
+    }
+
+    private class FolderIterator implements Iterator<Folder> {
+        private Iterator<Folder> currentIterator;
+        private FolderIterator subFolderIterator;
+        private Folder nextFolder;
+
+        public FolderIterator(List<Folder> rootFolders) {
+            this.currentIterator = rootFolders != null ? rootFolders.iterator() : null;
+            this.nextFolder = advance();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextFolder != null;
+        }
+
+        @Override
+        public Folder next() {
+            Folder toReturn = nextFolder;
+            nextFolder = advance();
+
+            return toReturn;
+        }
+
+        private Folder advance() {
+            // Check if there is a subfolder iterator that is not yet exhausted
+            if (subFolderIterator != null && subFolderIterator.hasNext()) {
+                return subFolderIterator.next();
+            }
+            subFolderIterator = null;
+
+            // Iterate over the current level folders
+            while (currentIterator != null && currentIterator.hasNext()) {
+                Folder currentFolder = currentIterator.next();
+
+                // If it's a MultiFolder, create a new iterator for its subfolders
+                if (currentFolder instanceof MultiFolder) {
+                    subFolderIterator = new FolderIterator(((MultiFolder) currentFolder).getFolders());
+                    if (subFolderIterator.hasNext()) {
+                        return currentFolder;
+                    }
+                } else {
+                    return currentFolder;
+                }
+            }
+            return null; // No more folders
+        }
     }
 }
 
